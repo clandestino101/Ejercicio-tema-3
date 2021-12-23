40be1dd4ea17fd3f5f8612496fb56dc2dce2ca80
clandestino101 <bragclandestino@gmail.com>
2021-12-23 20:22:33 +0100

    [Ejercicio Tema 3] Concatenar texto

---
+++ src/com/ejercicio3/EjercicioTema3.java
@@ -0,0 +1,15 @@
+package com.ejercicio3;
+
+public class EjercicioTema3 {
+
+    // Ejercicio tema 3 concatenar texto
+
+    public static void main(String[] args) {
+
+        String[] datos = new String[]{"Nombre,", "Apellido,", "Dirección,", "Ciudad,", "Pais."};
+        for (String dato : datos) {
+            System.out.print(dato + " ");
+        }
+
+    }
+}
